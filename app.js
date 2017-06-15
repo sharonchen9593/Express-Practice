29c8cdb69e2acf4c5e0c8bb7685c80b44d7dbf32
sharonchen9593 <sharonchen9593@gmail.com>
2017-06-14 23:36:11 -0700

    trying Querystring and Post Parameters

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser') // for post request. parse content of http to get it
 var app = express(); // invoke function from require('express')
 
 var port = process.env.PORT || 3000; // either equal to environment variable or 3000
+
+var urlencodedParser = bodyParser.urlencoded({extended:false})
+
+var jsonParser = bodyParser.json()
 
 app.use('/assets', express.static(__dirname + '/public')); // middleware for static files
 
@@ -21,8 +26,20 @@ app.get('/api', function(req, res) { // if GET method was requested on the url '
 });
 
 app.get('/person/:id', function(req, res) { // :id is a variable name
-  res.render('person', {ID: req.params.id}); // puts id into the params obj. render will send it to the person view file.
+  res.render('person', {ID: req.params.id, Qstr: req.query.qstr}); // puts id into the params obj. query object will have the api query string. render will send it to the person view file.
 });
+
+app.post('/person', urlencodedParser, function(req, res) {
+  res.send('Thank you!');
+  console.log(req.body.firstname);
+  console.log(req.body.lastname);
+});
+
+app.post('/personjson', jsonParser, function(req, res) {
+  res.send('Thank you for the json data');
+  console.log(req.body.firstname);
+  console.log(req.body.lastname);
+})
 
 app.listen(port); // combines the createServer and listen in nodejs
 
