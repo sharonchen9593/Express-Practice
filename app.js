51bdff8a6cc84a3ee057dfc386c0fcbee4d56a6a
sharonchen9593 <sharonchen9593@gmail.com>
2017-06-14 20:47:34 -0700

    added middleware for static files

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ var app = express(); // invoke function from require('express')
 
 var port = process.env.PORT || 3000; // either equal to environment variable or 3000
 
-app.get('/', function(req, res) { // if GET method was requested on the url '/'
-  res.send('<html><head></head><body><h1>Hello World</h1></body></html>')
+app.use('/assets', express.static(__dirname + '/public')); // middleware for static files
+
+app.get('/', function(req, res) { // if GET method was requested on the url '/' (routes)
+  res.send("<html><head><link href='assets/style.css' type='text/css' rel='stylesheet'/></head><body><h1>Hello World</h1></body></html>")
 });
 
 app.get('/api', function(req, res) { // if GET method was requested on the url '/api'
